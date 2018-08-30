752cdcea04bfa750fc7976cf86a2447ad0a9e6ae
James Dreiss <jamesdreiss@gmail.com>
2018-08-29 22:47:01 -0400

    add process_video

---
+++ process_video.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import logging
+import argparse
+import cv2
+
+
+path = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+def process_video(vid):
+    fps = 30
+    vidsize = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    writer = cv2.VideoWriter()
+    out = writer.open('/Users/jamesdreiss/Downloads/FacialRecognition/test.mp4', fourcc, fps, vidsize)
+
+    while vid.isOpened():
+        ret, frame = vid.read()
+        if ret is True:
+            frame = cv2.flip(frame, 0)
+
+            out.write(frame)
+
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    vid.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--vid', required=True)
+    args = parser.parse_args()
+
+    vid = cv2.VideoCapture(args.vid)
+    process_video(vid)
+
+
+if __name__ == '__main__':
+    main()
