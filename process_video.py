167445e74b21490afb9ee611e09bdabc447a1371
James Dreiss <jamesdreiss@gmail.com>
2018-08-31 09:27:54 -0400

    add rotate without cropping and then rotating a couple times

--- process_video.py
+++ process_video.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import argparse
 import time
+import numpy as np
 import cv2
 
 import mask_ai
@@ -13,21 +14,31 @@ path = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
-def rotate_frame(frame, degrees):
+def rotate_img(img, degrees):
     """
-    Return rotated numpy array image
+    Rotate an image without cropping it
 
-    :param frame: single frame of video as a numpy array
-    :param degrees: degrees of rotation, 0 - 360
+    :param img: single frame of video as a numpy array
+    :param degrees: degrees of desired counterclockwise rotation, 0 - 360
     :return: Rotated image as numpy array
     """
 
-    rows, cols = frame.shape[:2]
+    (h, w) = img.shape[:2]
+    (cx, cy) = (w // 2, h // 2)
 
-    m = cv2.getRotationMatrix2D((cols / 2, rows / 2), degrees, 1)
-    frame_rotated = cv2.warpAffine(frame, m, (cols, rows))
+    m = cv2.getRotationMatrix2D((cx, cy), degrees, 1.0)
+    cos = np.abs(m[0, 0])
+    sin = np.abs(m[0, 1])
 
-    return frame_rotated
+    new_w = int((h * sin) + (w * cos))
+    new_h = int((h * cos) + (w * sin))
+
+    m[0, 2] += (new_w / 2) - cx
+    m[1, 2] += (new_h / 2) - cy
+
+    img_rotated = cv2.warpAffine(img, m, (new_w, new_h))
+
+    return img_rotated
 
 
 def process_video(vid_name, vid_cap, vid_write):
@@ -40,20 +51,23 @@ def process_video(vid_name, vid_cap, vid_write):
     """
 
     start = time.time()
-
     counter = 0
+
     while vid_cap.isOpened():
         counter += 1
         ret, frame = vid_cap.read()
         if ret is True:
             logging.info('processing frame {} of {}'.format(counter, vid_name))
 
-            frame_mask = mask_ai.apply_mask(frame, polyline='full', colors=(255, 0, 255), line_thickness=3,
+            frame_v = rotate_img(frame, 270)
+            frame_mask = mask_ai.apply_mask(frame_v, polyline='full', colors=(255, 0, 255), line_thickness=3,
                                             detect_faces=False)
-            if frame_mask is not None:
-                vid_write.write(frame_mask)
+            frame_mask_h = rotate_img(frame_mask, 90)
+
+            if frame_mask_h is not None:
+                vid_write.write(frame_mask_h)
                 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-                cv2.imshow('frame', frame_mask)
+                cv2.imshow('frame', frame_mask_h)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
             else:
